215ed8b559f51296b361ace17dede006a7e188eb
Farhat Faiza <lfaizafarhat@gmail.com>
2026-03-30 04:40:29 +0000

    dim tables

--- models/assignment_work/mart/shared/dim_date.sql
+++ models/assignment_work/mart/shared/dim_date.sql
@@ -10,7 +10,7 @@ WITH all_dates AS (
 
    -- Get dates from restaurant applications
    SELECT DISTINCT CAST(time_of_submission AS DATE) AS full_date
-   FROM {{ ref('stg_nyc_open_restaurants_app') }}
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE time_of_submission IS NOT NULL
 ),
 
